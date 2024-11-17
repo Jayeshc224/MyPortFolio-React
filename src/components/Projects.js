67c2c343595c5c983aa4f903e8bc22dd07aae4fd
Jayeshc224 <chindarkar.j@northeastern.edu>
2024-11-17 10:53:03 -0500

    Updated Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,18 +10,18 @@ import "animate.css";
 export const Projects = () => {
     const projects = [
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "Movie Booking App with Suggestions",
+            description: "Designed and Developed the application for booking movies and suggest movies to users",
             imgUrl: projImg1
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "KanBan Board",
+            description: "A application to manage daily tasks and map the progress of each task",
             imgUrl: projImg2
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "Event Management App",
+            description: "Application designed to manage and host events to people",
             imgUrl: projImg3
         }
     ]
@@ -34,19 +34,21 @@ return (
           {({isVisible }) =>
           <div className = {isVisible ? "animated__animated animate__bounce" : ""}>
             <h2>Projects</h2>
-            <p>The projects featured here represent a broad range of skills across web design, backend development, and user experience design. Each project is crafted with a focus on both form and function, utilizing responsive layouts, intuitive UX, and efficient backend operations.</p>                   
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe</p>                   
             </div>}
         </TrackVisibility>
         <Tab.Container id="projects-tabs" defaultActiveKey ="first">
         <Nav variant="pills"  className="nav-pills mb-5 justify-content-center align-items-center" id="pill-tabs">
             <Nav.Item>
-                <Nav.Link eventKey="first">Web Projects</Nav.Link>
+                <Nav.Link eventKey="first">Tab One</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="second">UX Projects</Nav.Link>
+                <Nav.Link eventKey="second">Tab Two</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="third" >Java Projects</Nav.Link>
+                <Nav.Link eventKey="third" >
+                Tab Three
+                </Nav.Link>
             </Nav.Item>
             </Nav>
             <Tab.Content>
@@ -62,10 +64,10 @@ return (
                         }
                     </Row>
                 </Tab.Pane>
-                <Tab.Pane eventKey="second"></Tab.Pane>
-                <Tab.Pane eventKey="third"></Tab.Pane> 
+                <Tab.Pane eventKey="second">Loren ipsum</Tab.Pane>
+                <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
             </Tab.Content>
-            <img className="background-image-right" src={colorSharp2} alt="JAC" /> 
+            <img className="background-image-right" src={colorSharp2} /> 
             </Tab.Container>
 
         </Col>
